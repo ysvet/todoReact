f58cce3a2f76aaac08137809e0af1d5b1a4120aa
Yuri Svet <iurie.shvets@gmail.com>
2018-12-20 11:47:37 +0200

    added search

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -16,8 +16,9 @@ export default class App extends Component {
       this.createTodoItem('Drink coffe'),
       this.createTodoItem('Make Awesome App'),
       this.createTodoItem('Have a lunch'),
-      ]
-  };
+    ],
+    searchfield: ''
+    };
 
   createTodoItem(label) {
     return {
@@ -93,22 +94,34 @@ onToggleImportant = (id) => {
 
 };
 
+  onSearchChange = (searchfield) => {
+    this.setState({ searchfield })
+  };
+
+  search = (items, searchfield) => {
+    if (searchfield === 0) {
+      return items
+    };
+      return items.filter((item) => item.label.toLowerCase().includes(searchfield.toLowerCase()))
+  };
 
 render () {
 
-  const { todoData } = this.state;
+  const { todoData, searchfield } = this.state;
   const doneCount = todoData.filter((el) => el.done).length; // el.done -the same as- el.done === true;
   const todoCount = todoData.length - doneCount;
+
+  const visibleItems = this.search(todoData, searchfield);
 
   return (
     <div className="todo-app">
       <AppHeader toDo={todoCount} done={doneCount} />
       <div className="top-panel d-flex">
-        <SearchPanel />
+        <SearchPanel  onSearchChange = { this.onSearchChange }/>
         <ItemStatusFilter />
       </div>
 
-      <TodoList todos = { todoData }
+      <TodoList todos = { visibleItems }
        onDeleted = { this.deleteItem }
        onToggleImportant = { this.onToggleImportant }
        onToggleDone = { this.onToggleDone }
--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './search-panel.css';
 
-const SearchPanel = () => {
+export default class SearchPanel extends Component {
+
+  state = {
+    searchfield: ''
+  };
+
+ onSearchChange = (e) => {
+   const searchfield = e.target.value;
+   this.setState({searchfield});
+   this.props.onSearchChange(searchfield);
+ };
+
+  render() {
   return (
     <input type="text"
               className="form-control search-input"
-              placeholder="type to search" />
+              placeholder="type to search"
+              value = {this.state.searchfield}
+              onChange = {this.onSearchChange} />
   );
+ }
 };
-
-export default SearchPanel;
